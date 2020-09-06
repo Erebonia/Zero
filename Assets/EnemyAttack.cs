9c4a571890ed6337359ac3b7c32345ec3a97f583
deathhopes <52137104+deathhopes@users.noreply.github.com>
2020-09-05 23:46:19 -1000

    Enemy HP and ATTACK

---
+++ Assets/EnemyAttack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public int damagePower = 1;
+    
+    public float attackCooldown = 1f;
+    private float nextAttackTime = 0f;
+
+    void OnTriggerStay2D(Collider2D player)
+    {
+        if (Time.time >= nextAttackTime)
+        {
+            player.GetComponent<Player>().TakeDamage(damagePower);
+            nextAttackTime = Time.time + 1f / attackCooldown;
+        }
+        
+    }
+}
