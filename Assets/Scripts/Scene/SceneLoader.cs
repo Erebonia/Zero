482748f368caa957ce8fe360286213b600a09f09
deathhopes <52137104+deathhopes@users.noreply.github.com>
2020-09-06 20:19:11 -1000

    Additive Scene Loading

---
+++ Assets/Scripts/Scene/SceneLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Animator anim;
+
+    public float transitionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            LoadNextScene();
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        StartCoroutine(LoadScene(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadScene(int loadIndex)
+    {
+        //Play fade animation.
+        anim.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load New
+        SceneManager.LoadScene(loadIndex, LoadSceneMode.Additive);
+    }
+}
