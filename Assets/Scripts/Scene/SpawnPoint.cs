9d799942ee983876a893f5e12c16044fa887006f
deathhopes <52137104+deathhopes@users.noreply.github.com>
2020-09-07 15:37:04 -1000

    Basic spawn points

---
+++ Assets/Scripts/Scene/SpawnPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    GameObject player;
+
+    void Awake()
+    {
+        player = GameObject.Find("Player");
+        player.transform.position = gameObject.transform.position;
+    }
+}
